dfbdec0300bccda32f8bd6281eacfe84c78b8e02
Patrii <fioselingates@gmail.com>
2020-12-19 13:02:43 +0100

    Patrii Y Luu incorporado
    Incorporado bien

--- Code/HellProject/Assets/Scripts/LevelManager.cs
+++ Code/HellProject/Assets/Scripts/LevelManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class LevelManager : MonoBehaviour
 {
+    #region variables
     public bool isQuestionVisible;
     public bool isQuestionable;
     [SerializeField] private List<Monument> monuments;
@@ -11,15 +12,45 @@ public class LevelManager : MonoBehaviour
 
     public static LevelManager instance;
 
+    public Demon littleDemon;
+    private int numDemons = 0;
+    public List<GameObject> tourists = null;
+    private float spawnTime = 2.0f;
+    public List<GameObject> spawnPoint;
+    private System.Random r;
+    #endregion
+    #region methods
+    
     private void Awake()
     {
         if (instance) Destroy(instance);
         instance = this;
         currentMonument = monuments[0];
+        spawnPoint = GameObject.FindGameObjectsWithTag("SpawnPoint").ToList<GameObject>();
+        InvokeRepeating("spawner", 1, spawnTime);
+        tourists = GameObject.FindGameObjectsWithTag("Turista").ToList<GameObject>();
+        r = new System.Random();
+
     }
 
     public void UpdateMonument()
     {
         Debug.Log("Level Manager: Monument Updated!");
     }
+
+    private void spawner()
+    {
+        numDemons = GameObject.FindGameObjectsWithTag("Demon").Length;
+        foreach (GameObject point in spawnPoint)
+        {
+            if (numDemons < 5 && tourists.Count > 0)
+            {
+                Demon aux = Instantiate(littleDemon, point.transform.position, point.transform.rotation);
+                aux.SetHome(point.transform.position);
+                numDemons++;
+            }
+
+        }
+    }
+    #endregion
 }
--- Code/HellProject/Assets/Scripts/Tourist.cs
+++ Code/HellProject/Assets/Scripts/Tourist.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class Tourist : MonoBehaviour
 {
@@ -9,4 +12,101 @@ public class Tourist : MonoBehaviour
     [HideInInspector] public int rightAnswers = 0;
     [HideInInspector] public int wrongAnswers = 0;
     [HideInInspector] public int emptyAnswers = 0;
+    // Start is called before the first frame update
+    #region variables
+
+    #region variablesUpdatedManager
+    private string name;
+    string path = "Assets/TXT/test.txt";
+    private TextAsset genericQuestions;
+    private TextAsset monumentQuestions;
+    private TextAsset emptyRating;
+    private List<Question> currentQuestions;
+    #endregion
+
+    #region variablesUpdatedGame
+    public bool targeted;
+    public bool kidnapped;
+    private bool dying;
+    private float questionsCooldown;
+    private float happiness;
+    #endregion
+    #endregion
+
+    #region methods
+    void Start()
+    {
+        targeted = false;
+        dying = false;
+        kidnapped = false;
+        happiness = 1.0f;
+        rightAnswers = 0;
+        wrongAnswers = 0;
+        emptyAnswers = 0;
+     //   ReadString(path, name);
+
+    }
+    void Update()
+    {
+        die();
+    }
+    public void GenerateQuestions()
+    {
+
+    }
+
+    public void AskQuestion()
+    {
+
+    }
+    public void GenerateRating()
+    {
+
+    }
+  /*  static void ReadString(String path, String name)
+    {
+        StreamReader reader = new StreamReader(path);
+        name = reader.ReadLine();
+        Debug.Log(name);
+        reader.Close();
+    }*/
+    public void Leave()
+    { // cuanto de felicidad se va el personaje
+
+    }
+    public void die()
+    {
+        if (dying)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+    #endregion
+    #region getters and setters
+    public bool GetTargeted()
+    {
+        return targeted;
+    }
+    public void SetTargeted(bool newTargeted)
+    {
+        targeted = newTargeted;
+    }
+    public bool GetKidnapped()
+    {
+        return kidnapped;
+    }
+    public void SetKidnapped(bool newKidnapped)
+    {
+        kidnapped = newKidnapped;
+    }
+    public bool GetDying()
+    {
+        return dying;
+    }
+    public void SetDying(bool newDying)
+    {
+        dying = newDying;
+    }
+    #endregion
 }
