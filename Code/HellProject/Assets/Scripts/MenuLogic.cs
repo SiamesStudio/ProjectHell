4d6c9ce7fd9e3be38f316238019e5c15353f8c37
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2020-12-27 11:41:44 +0100

    Starting with menu & Responsive

---
+++ Code/HellProject/Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuLogic : MonoBehaviour
+{
+    [SerializeField] Button chaptersButton;
+    [SerializeField] Button bundlesButton;
+    [SerializeField] Button coinsButton;
+    [SerializeField] Button playButton;
+    [SerializeField] GameObject shopUI;
+    [SerializeField] GameObject upgradesUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChaptersButton()
+    {
+
+    }
+
+    public void OnBundlesButton()
+    {
+
+    }
+
+    public void OnCoinsButton()
+    {
+
+    }
+
+    public void OnPlayButton()
+    {
+
+    }
+
+    public void OnMenuButton()
+    {
+
+    }
+
+    public void OnNextButton()
+    {
+
+    }
+
+}
