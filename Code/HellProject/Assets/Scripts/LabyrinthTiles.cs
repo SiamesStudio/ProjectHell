611ecf6b84344eeb363cecbd16da54d12329e5f7
zuuhr <zuuhr@users.noreply.github.com>
2021-01-01 16:08:16 +0100

    Laberintos shader

---
+++ Code/HellProject/Assets/Scripts/LabyrinthTiles.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthTiles : MonoBehaviour
+{
+    [Header("Lava direction")]
+    [SerializeField] private bool _IsLava;
+    [SerializeField] private bool _LavaTop;
+    [SerializeField] private bool _LavaRight;
+    [SerializeField] private bool _LavaBottom;
+    [SerializeField] private bool _LavaLeft;
+    [SerializeField] private bool _LavaTopRightCorner;
+    [SerializeField] private bool _LavaBottomRightCorner;
+    [SerializeField] private bool _LavaBottomLeftCorner;
+    [SerializeField] private bool _LavaTopLeftCorner;
+
+    static readonly int material_IsLava = Shader.PropertyToID("_IsLava");
+    static readonly int material_LavaTop = Shader.PropertyToID("_LavaTop");
+    static readonly int material_LavaRight = Shader.PropertyToID("_LavaRight");
+    static readonly int material_LavaBottom = Shader.PropertyToID("_LavaBottom");
+    static readonly int material_LavaLeft = Shader.PropertyToID("_LavaLeft");
+    static readonly int material_LavaTopRightCorner = Shader.PropertyToID("_LavaTopRightCorner");
+    static readonly int material_LavaBottomRightCorner = Shader.PropertyToID("_LavaBottomRightCorner");
+    static readonly int material_LavaBottomLeftCorner = Shader.PropertyToID("_LavaBottomLeftCorner");
+    static readonly int material_LavaTopLeftCorner = Shader.PropertyToID("_LavaTopLeftCorner");
+    static Material material;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+
+        if (_IsLava)
+        {
+            material.SetInt(material_IsLava, 1);
+            return;
+        }
+        if (_LavaTop)               material.SetInt(material_LavaTop, 1);
+        if (_LavaRight)             material.SetInt(material_LavaRight, 1);
+        if (_LavaBottom)            material.SetInt(material_LavaBottom, 1);
+        if (_LavaLeft)              material.SetInt(material_LavaLeft, 1);
+        if (_LavaTopRightCorner)    material.SetInt(material_LavaTopRightCorner, 1);
+        if (_LavaBottomRightCorner) material.SetInt(material_LavaBottomRightCorner, 1);
+        if (_LavaBottomLeftCorner)  material.SetInt(material_LavaBottomLeftCorner, 1);
+        if (_LavaTopLeftCorner)     material.SetInt(material_LavaTopLeftCorner, 1);
+    }
+
+}
---
+++ Code/HellProject/Assets/Scripts/Maru/LabyrinthRenderer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthRenderer : MonoBehaviour
+{
+
+    [SerializeField] private int _width = 0;
+    [SerializeField] private int _height = 0;
+    
+    private int[,] labyrinth;
+
+    public TextAsset fileName;
+    //private ArrayList<Tile> tiles;
+
+    void Start()
+    {
+        string[] textString = fileName.text.Split(new string[] { " ", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+        int numNodes = int.Parse(textString[0]);
+    }
+
+    private void generateLabyrinth(int[,] labyrinth)
+    {
+        //Genero la posición incial del player
+        //Vector2Int playerPosition = new Vector2Int(UnityEngine.Random.Range(0, _height), UnityEngine.Random.Range(0, _width));
+        Vector2Int playerPosition = new Vector2Int(UnityEngine.Random.Range(0, _height), UnityEngine.Random.Range(0, _width));
+        labyrinth[playerPosition.x, playerPosition.y] = 0;
+
+        
+
+
+    }
+
+    private void setRenderType(int[,] labyrinth, int i, int j)
+    {
+
+    }
+
+    private void rasterizeLabyrinth(int[,] dungeon)
+    {
+        for (int i = 0; i < _height; i++)
+            for (int j = 0; j < _width; j++)
+                if (dungeon[i, j] == 1)
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(j - (_width / 2), 0, i - (_height / 2));
+                }
+    }
+
+}
+
+
+class Tile
+{
+    //0 land, 1 lava
+    int colliderType { get; set; }
+
+    //lava positions
+    bool top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft;
+
+
+    Tile(int colliderType)
+    {
+        this.colliderType = colliderType;
+    }
+}
