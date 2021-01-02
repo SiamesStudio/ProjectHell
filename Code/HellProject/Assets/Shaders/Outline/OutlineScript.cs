44412c45b7e0a6db278eb86c6cc2716aeba91580
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2021-01-02 18:39:49 +0100

    Outline shader
    PUSH con el shader Outline. En la carpeta de Shaders se ha añadido la subcarpeta "Outline" con un script "OutlineScript", un material de ejemplo "OutlineDemon" y un shader "Outline". El objeto debe recibir el script y el script a su vez recibe el material.
    
    Hay dos problemas: ahora mismo funciona con meshes, no con conjunto de meshes (y creo que eso no es viable ahora mismo). El segundo problema es que no funciona con objetos animados, pues el outline crea una mesh extra con un culling reverso. Habría que añadirle un animator y demás. Sólo sirve para objetos interactivos estáticos.
    
    Como solución, propongo instanciar una flecha encima del demonio sobre el que se sitúa el cursor.

---
+++ Code/HellProject/Assets/Shaders/Outline/OutlineScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineScript : MonoBehaviour
+{
+    private GameObject outlineObject;
+    [SerializeField] Material outlineMaterial;
+    [SerializeField] bool debugShortCuts;
+    private Renderer renderer;
+
+
+    private void Start()
+    {
+        renderer = CreateOutline(outlineMaterial);
+    }
+
+    private void Update()
+    {
+        if (!debugShortCuts) return;
+        if (Input.GetKeyDown(KeyCode.Space))
+            SetVisible(!renderer.enabled);
+    }
+
+    private Renderer CreateOutline(Material _material)
+    {
+        outlineObject = Instantiate(new GameObject(), transform.position, transform.rotation, transform);
+        outlineObject.AddComponent<MeshFilter>();
+        outlineObject.AddComponent<MeshRenderer>();
+        outlineObject.GetComponent<MeshFilter>().mesh = GetComponent<MeshFilter>().mesh;
+
+        outlineObject.GetComponent<Renderer>().material = outlineMaterial;
+        outlineObject.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        outlineObject.GetComponent<Renderer>().enabled = false;
+
+        return outlineObject.GetComponent<Renderer>();
+    }
+
+    public void SetVisible(bool _isVisible)
+    {
+        renderer.enabled = _isVisible;
+    }
+}
