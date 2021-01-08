16dc93adbabd6a5ad8c4b19cb53206158c360466
CinnamonLuu <lucia.ortuno22@gmail.com>
2021-01-08 10:42:38 +0100

    Animacion fade para cambios de pantalla

---
+++ Code/HellProject/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToLevel(int level)
+    {
+        levelToLoad = level;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeOutComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
