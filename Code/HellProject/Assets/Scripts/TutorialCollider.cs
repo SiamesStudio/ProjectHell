99a6d5feb49fe088e4cb0532b418385d26838470
José Luis Murcia Gámez <alaricoperchitaf10@gmail.com>
2021-01-11 12:29:07 +0100

    TUTORIAL PROTOTYPE?¿

---
+++ Code/HellProject/Assets/Scripts/TutorialCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCollider : MonoBehaviour
+{
+    [SerializeField] TutorialHolder tutorialHolder;
+    [SerializeField] private Collider boxCollider;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        TutorialStep step = tutorialHolder.steps.Find((x) => x.trigger == boxCollider);
+        if (step == null) return;
+        tutorialHolder.DisplayText(step);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        TutorialStep step = tutorialHolder.steps.Find((x) => x.trigger == boxCollider);
+        if (step == null) return;
+        tutorialHolder.HideText(step);
+    }
+}
---
+++ Code/HellProject/Assets/Scripts/TutorialHolder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialHolder : MonoBehaviour
+{
+    [SerializeField] RawImage image;
+    [SerializeField] Text screenText;
+    [SerializeField] string[] strings;
+    [SerializeField] List<Collider> triggers = new List<Collider>();
+    public List<TutorialStep> steps = new List<TutorialStep>();
+
+    private void Start()
+    {
+        
+        image.gameObject.SetActive(false);
+        for(int i=0; i< strings.Length; i++)
+        {
+            steps.Add(new TutorialStep(strings[i], triggers[i]));
+        }
+    }
+
+    public void DisplayText(TutorialStep step)
+    {
+        screenText.text = step.text;
+        image.gameObject.SetActive(true);
+        screenText.gameObject.SetActive(true);
+    }
+
+    public void HideText(TutorialStep step)
+    {
+        image.gameObject.SetActive(false);
+        screenText.gameObject.SetActive(false);
+    }
+
+    
+}
+
+public class TutorialStep
+{
+    public string text;
+    public Collider trigger;
+
+    public TutorialStep(string _text, Collider _trigger)
+    {
+        text = _text; trigger = _trigger;
+    }
+}
